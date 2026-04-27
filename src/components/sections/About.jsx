4f32feaf05df390b72252e10c77cbf327cc45488
Nicolas Rodriguez <nicolasrp432@gmail.com>
2026-04-27 14:20:42 +0200

    Replace image source with local file

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -217,7 +217,7 @@ const About = () => {
             viewport={{ once: true }}
             transition={{ duration: 0.8 }}
           >
-            <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="Nicolas Rodriguez" />
+            <img src="/nicolas.png" alt="Nicolas Rodriguez" />
             <AboutImageBorder />
           </AboutImage>
           
