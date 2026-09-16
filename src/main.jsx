18d4d44e730983fe8f130a4fbce75101f74b1e26
Nicolas Rodriguez <nicolasrp432@gmail.com>
2026-09-16 17:43:01 +0200

    Redesign portfolio around real GitHub projects

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import reportWebVitals from './reportWebVitals.jsx'
 
-// Import Google Fonts
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap';
-document.head.appendChild(link);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
